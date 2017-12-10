b3c53a48a561c24340381bc8fb5bca9c973ae3ea
Markus Kupke <kupkemarkus@gmail.com>
2017-12-10 17:36:44 +0100

    Helper functions

--- WS2801_RPI.py
+++ WS2801_RPI.py
@@ -5,25 +5,56 @@ spidev can be found here: https://github.com/doceme/py-spidev.
 """
 import spidev
 import logging
+import time
 
 _BUS = 0
 _DEVICE = 0
 _MAX_SPEED_HZ = 976000
-_LEDS = 128
+_LEDS = 0
 __rgb_leds = []
+_mode = 0b00
 
+# texts to separate into resources once I do a package (if ever)
+__string_type_error_1 = "Attribute pixels must be an \
+int addressing one of the available LEDs or a list of ints"
+__string_type_error_2 = "Attribute rgb_values must be an \
+list containing 3 ints between 0 and 255 or a list of lists with rgb values"
+__string_type_error_3 = "Attribute must be a dictionary"
+__string_type_error_4 = "Value must be a dictionary as well"
+__string_type_error_5 = "rgb values need to be ints and between 0 and 255"
+__string_runtime_error_1 = "You are addressing an LED that does not exist"
+__string_runtime_error_2 = "Something weird happend: Buffer overflow"
+__string_runtime_error_3 = "SPI Bus must be driven with clock speed > 1500HZ \
+for WS2801"
+__string_runtime_error_4 = "Mode must be between 0 and 3."
+__string_warning_1 = "more leds addressed than rgb \
+values given: assume last rgb for remaining leds"
+__string_warning_2 = "too many rgb values supplied: \
+skipping"
 
 # initialize SPI
 # this should meet the requirements of WS2801
 try:
     spi = spidev.SpiDev()
     spi.open(bus=_BUS, device=_DEVICE)
-    spi.mode = 0b00
+    spi.mode = _mode
     spi.max_speed_hz = _MAX_SPEED_HZ
 except:
     import traceback
     traceback.print_exc()
     raise
+
+
+def set_spidev_bus_device(bus=0, device=0):
+    try:
+        spi.close()
+        spi.open(bus, device)
+        spi.mode = _mode
+        spi.max_speed_hz = _MAX_SPEED_HZ
+    except:
+        import traceback
+        traceback.print_exc()
+        raise
 
 
 def set_number_of_leds(leds=128):
@@ -51,15 +82,76 @@ def flush():
         traceback.print_exc()
         raise
     return
+    # WS2801 needs 500 microseconds clock low before next data transfer
+    time.sleep(.0005)
 
 
 def clear():
-    "Switch all leds off. Needs a flush() to be active."
+    """Switch all leds off. Needs a flush() to be active."""
     global __rgb_leds
     __rgb_leds[:] = [0 for i in range(_LEDS*3)]
 
 
-def set_leds(pixels, rgb_values=[255, 255, 255]):
+def get_led_colors_buffer_dict():
+    """
+    Get the buffer for leds colors as dict.
+
+    The leds are indexed with ints starting with 1 and values are dicts with
+    rgb indexed with "red","green" and "blue"
+    """
+    global _LEDS
+    leds_dict = {}
+    rgb_dict = {"red": 0, "green": 0, "blue": 0}
+    for index in range(0, _LEDS * 3, 3):
+        rgb_dict = {"red": __rgb_leds[index],
+                    "green": __rgb_leds[index+1],
+                    "blue": __rgb_leds[index+2]}
+        leds_dict[(index+3)/3] = rgb_dict
+    return leds_dict
+
+
+def set_led_colors_buffer_dict_multi_call(leds_dict):
+    """
+    Set the buffer for the leds via a dictionary.
+
+    Key of dictionary is an int adressing the number of the led,
+    all other keys are ignored.
+    Value is a dictionary with the keys "red", "green" and "blue".
+    Other values are ignored. If a color key is not found it defaults to 0.
+    Values in the second dictinary can be ints between 0 and 255
+    """
+    if not type(leds_dict) is dict:
+        raise TypeError(__string_type_error_3)
+    for led, rgb_dict in leds_dict.iteritems():
+        rgb_list = [0, 0, 0]
+        if type(led) is int:
+            if led < 1 or led > _LEDS:
+                raise TypeError(__string_runtime_error_1)
+            if not type(rgb_dict) is dict:
+                raise TypeError(__string_type_error_4)
+            for color, rgb_value in rgb_dict.iteritems():
+                if color == "red":
+                    if (not type(rgb_value) is int or
+                            rgb_value < 0 or
+                            rgb_value > 255):
+                        raise TypeError(__string_type_error_5)
+                    rgb_list[0] = rgb_value
+                if color == "green":
+                    if (not type(rgb_value) is int or
+                            rgb_value < 0 or
+                            rgb_value > 255):
+                        raise TypeError(__string_type_error_5)
+                    rgb_list[1] = rgb_value
+                if color == "blue":
+                    if (not type(rgb_value) is int or
+                            rgb_value < 0 or
+                            rgb_value > 255):
+                        raise TypeError(__string_type_error_5)
+                    rgb_list[2] = rgb_value
+            set_led_colors_buffer_list_multi_call(led, rgb_list)
+
+
+def set_led_colors_buffer_list_multi_call(pixels, rgb_values=[255, 255, 255]):
     """Set a list of pixels to list of corresponding rgb_values (given as
     list of 3 values for red, green and blue).
 
@@ -73,38 +165,30 @@ def set_leds(pixels, rgb_values=[255, 255, 255]):
     global __rgb_leds
     if not type(pixels) is int:
         if not type(pixels) is list:
-            raise TypeError("WS2801_RPI.set_pixels attribute pixels must be an \
-int addressing one of the available LEDs or a list of ints")
+            raise TypeError(__string_type_error_1)
         for val in pixels:
             if (not type(val) is int) or val < 1 or val > _LEDS:
-                raise TypeError("WS2801_RPI.set_pixels attribute pixels must \
-be an int addressing one of the available LEDs or a list of ints")
+                raise TypeError(__string_type_error_1)
     else:
         if pixels > _LEDS or pixels < 1:
-            raise TypeError("You are addressing an LED that does not exist")
+            raise RuntimeError(__string_runtime_error_1)
     if not type(rgb_values) is list:
-        raise TypeError("WS2801_RPI.set_pixels attribute rgb_values must be an \
-list containing 3 ints between 0 and 255 or a list of lists with rgb values")
+        raise TypeError(__string_type_error_2)
     if type(rgb_values[0]) is list:
         for rgb_value in rgb_values:
             if not type(rgb_value) is list:
-                raise TypeError("WS2801_RPI.set_pixels attribute rgb_values \
-must be an list containing 3 ints for rgb or alist of lists with rgb values")
+                raise TypeError(__string_type_error_2)
             if not len(rgb_value) == 3:
-                raise TypeError("WS2801_RPI.set_pixels attribute rgb_values \
-must be an list containing 3 ints for rgb or a list of lists with rgb values")
+                raise TypeError(__string_type_error_2)
             for i in rgb_value:
                 if (not type(i) is int) or i < 0 or i > 255:
-                    raise TypeError("WS2801_RPI.set_pixels attribute rgb_values \
-must be an list containing 3 ints for rgb or a list of lists with rgb values")
+                    raise TypeError(__string_type_error_2)
     else:
         if not len(rgb_values) == 3:
-            raise TypeError("WS2801_RPI.set_pixels attribute rgb_values must \
-be an list containing 3 ints for rgb or a list of lists with rgb values")
+            raise TypeError(__string_type_error_2)
         for i in rgb_values:
             if (not type(i) is int) or i < 0 or i > 255:
-                raise TypeError("WS2801_RPI.set_pixels attribute rgb_values \
-must be an list containing 3 ints for rgb or a list of lists with rgb values")
+                raise TypeError(__string_type_error_2)
 
     # write into __rgb_leds
     if type(pixels) is int:
@@ -120,13 +204,11 @@ must be an list containing 3 ints for rgb or a list of lists with rgb values")
 
     # some final sanity checks
     if len(__rgb_leds) != _LEDS*3:
-        raise RuntimeError("Something weird happend: Buffer overflow")
+        raise RuntimeError(__string_runtime_error_2)
     if len(pixels) > len(rgb_values):
-        logging.warn("WS2801_RPI.py set_leds: more leds addressed than rgb \
-values given: assume last rgb for remaining leds")
+        logging.warn(__string_warning_1)
     if len(pixels) < len(rgb_values):
-        logging.warn("WS2801_RPI.py set_leds: too many rgb values supplied: \
-skipping")
+        logging.warn(__string_warning_2)
 
 
 def set_max_speed_hz(hz):
@@ -136,6 +218,11 @@ def set_max_speed_hz(hz):
     Default: 976000HZ. There is no need to call this function if default is ok.
     Consult devspi docu https://github.com/doceme/py-spidev.
     """
+    if hz <= 1500:
+        # 500 micro sec clock low is the time WS2801 needs to reset. hence
+        # it must be driven with a clock higher than 1000hz otherwise it will
+        # reset all the time :-)
+        raise RuntimeError(__string_runtime_error_3)
     try:
         spi.max_speed_hz = hz
     except:
@@ -154,12 +241,16 @@ def set_mode(mode):
     Check docu of spidev:
     "mode - SPI mode as two bit pattern of clock polarity and phase [CPOL|CPHA], min: 0b00 = 0, max: 0b11 = 3"
     """
+    if mode < 0 or mode > 3:
+        raise RuntimeError(__string_runtime_error_4)
     try:
         spi.mode = mode
     except:
         import traceback
         traceback.print_exc()
         raise
+    global _mode
+    _mode = mode
 
 
 def set_threewire(bool):
@@ -170,26 +261,6 @@ def set_threewire(bool):
     """
     try:
         spi.threewire = bool
-    except:
-        import traceback
-        traceback.print_exc()
-        raise
-
-
-def set_lbsfirst(bool):
-    """
-    Use only in case of panic.
-
-    Consult devspi docu https://github.com/doceme/py-spidev.
-    """
-    try:
-        spi.threewire = bool
-    except:
-        import traceback
-        traceback.print_exc()
-        raise
-    try:
-        spi.lsbfirst = bool
     except:
         import traceback
         traceback.print_exc()
