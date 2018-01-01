0f968171227af17e2c3d162eb64dfec048f4c78a
Markus Kupke <kupkemarkus@gmail.com>
2018-01-01 18:21:22 +0100

    build issues fixed

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    # py_modules=["WS2801_RPI"],
+    py_modules=["WS2801_RPI"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
