91207ae01ba2ed98c6fdfb3546fa97c9c34e31ce
Markus Kupke <kupkemarkus@gmail.com>
2018-01-02 14:42:04 +0100

    Samples reworked

--- samples/rainbow_banner.py
+++ samples/rainbow_banner.py
@@ -13,17 +13,13 @@ velocity = 300
 Leds.set_gamma(3)
 s = Leds.set_led_colors_buffer_list_multi_call
 
-for i in range(128):
-    red, green, blue = colorsys.hsv_to_rgb(i/granularity, 1, 1)
-    s(i+1, [int(red * 255), int(green * 255), int(blue * 255)])
-
-i = 127
+i = 0
 while True:
-   i = (i + 1) % int(granularity)
-   rgb = Leds.get_led_colors_buffer_list()
-   red, green, blue = colorsys.hsv_to_rgb(i/granularity, 1, 1)
-   rgb.insert(0, [int(red * 255), int(green * 255), int(blue * 255)])
-   rgb.pop()
-   s([(j+1) for j in range(128)], rgb)
-   Leds.flush()
-   time.sleep(1.0/velocity)
+    i = (i + 1) % int(granularity)
+    rgb = Leds.get_led_colors_buffer_list()
+    red, green, blue = colorsys.hsv_to_rgb(i/granularity, 1, 1)
+    rgb.insert(0, [int(red * 255), int(green * 255), int(blue * 255)])
+    rgb.pop()
+    s([(j+1) for j in range(128)], rgb)
+    Leds.flush()
+    time.sleep(1.0/velocity)
--- samples/snow.py
+++ samples/snow.py
@@ -4,8 +4,7 @@ from random import randint
 led.clear()
 led.flush()
 while True:
-    n=randint(1,128)
-    h=randint(0,255)
-    led.set_led_colors_buffer_list_multi_call(n,[h,h,h])
+    n = randint(1, 128)
+    h = randint(0, 255)
+    led.set_led_colors_buffer_list_multi_call(n, [h, h, h])
     led.flush()
-
