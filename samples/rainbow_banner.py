929240880dbb4fc88a4d64b7c416161ca7b4db57
Markus Kupke <kupkemarkus@gmail.com>
2018-01-02 14:35:41 +0100

    New Samples

---
+++ samples/rainbow_banner.py
@@ -0,0 +1,29 @@
+"""This is an example on how to use WS2801_RPI."""
+
+import WS2801_RPI as Leds
+import time
+import logging
+import colorsys
+
+granularity = 180.0
+
+
+logging.getLogger().setLevel(31)
+velocity = 300
+Leds.set_gamma(3)
+s = Leds.set_led_colors_buffer_list_multi_call
+
+for i in range(128):
+    red, green, blue = colorsys.hsv_to_rgb(i/granularity, 1, 1)
+    s(i+1, [int(red * 255), int(green * 255), int(blue * 255)])
+
+i = 127
+while True:
+   i = (i + 1) % int(granularity)
+   rgb = Leds.get_led_colors_buffer_list()
+   red, green, blue = colorsys.hsv_to_rgb(i/granularity, 1, 1)
+   rgb.insert(0, [int(red * 255), int(green * 255), int(blue * 255)])
+   rgb.pop()
+   s([(j+1) for j in range(128)], rgb)
+   Leds.flush()
+   time.sleep(1.0/velocity)
